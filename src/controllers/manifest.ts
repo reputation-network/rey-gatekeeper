c6d5b4244b1ea10ae24c53c53524bde7d3917e7e
Juan Jose Herrero <jherrero@traity.com>
2018-09-11 08:34:46 +0000

    Use logger on manifest controllers

--- src/controllers/manifest.ts
+++ src/controllers/manifest.ts
@@ -18,18 +18,19 @@ export default function makeManifestController(opts: IManifestControllerOptions)
   }
   const url = URL.parse(opts.manifestUrl);
   if (url.protocol === "file:") {
-    return fileManifestController(url.pathname || "");
+    return fileManifestController(url.pathname || "", opts.logger);
   } else if (url.protocol === "http:" || url.protocol === "https:") {
-    return httpManifestController(url);
+    return httpManifestController(url, opts.logger);
   } else {
     throw new Error(`Unkown manifest protocol: ${url.protocol || null}`);
   }
 }
 
-export function fileManifestController(path: string): RequestHandler {
+export function fileManifestController(path: string, logger: Logger): RequestHandler {
   return (req, res, next) => {
     readFile(path, "utf8", (err, result) => {
       if (err) {
+        logger.error(`Error while reading manifets file at ${path}: ${err}`);
         next(new HttpError(404, `Error reading the manifest file at '${path}': ${err.message || err}`));
       } else {
         res.setHeader("content-type", "application/json; charset=utf-8");
@@ -39,8 +40,10 @@ export function fileManifestController(path: string): RequestHandler {
   };
 }
 
-export function httpManifestController(url: URL.Url): RequestHandler {
+export function httpManifestController(url: URL.Url, logger: Logger): RequestHandler {
   const proxyOptions: Partial<proxy.Config> = {
+    logLevel: logger.level as any,
+    logProvider: () => logger,
     target: url.href,
     xfwd: true,
   };
